6af902ab83dafb55380bb54a6c5357b711a24932
jonghwan83 <jonghwan.park83@gmail.com>
2022-01-12 22:31:05 +0900

    solved

--- acmicpc/acmicpc10989.py
+++ acmicpc/acmicpc10989.py
@@ -5,4 +5,13 @@ lst = []
 for _ in range(T):
     lst.append(int(sys.stdin.readline()))
 
-print(lst)
+index = [i for i in range(max(lst)+1)]
+count = [0 for _ in range(max(lst)+1)]
+
+for val in lst:
+    count[val] += 1
+
+for i in range(1, len(count)):
+    count[i+1] += count[i]
+
+print(count)
