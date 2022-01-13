30ae1b3bac19069ebc9f4bb2585532c6239931d0
jonghwan83 <jonghwan.park83@gmail.com>
2022-01-14 00:05:04 +0900

    solved

--- acmicpc/acmicpc01427.py
+++ acmicpc/acmicpc01427.py
@@ -5,7 +5,6 @@ count = [0 for _ in range(10)]
 for s in str(n):
     count[int(s)] += 1
 output = ''
-
 for i in range(len(count)-1, -1, -1):
     if count[i] != 0:
         output += str(i) * count[i]
