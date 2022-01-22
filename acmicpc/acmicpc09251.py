520da0be661f32676eb049fd55ec1d0db9e93d14
jonghwan83 <jonghwan.park83@gmail.com>
2022-01-22 10:31:19 +0900

    solved

--- acmicpc/acmicpc09251.py
+++ acmicpc/acmicpc09251.py
@@ -3,14 +3,6 @@ LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열
 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
 
 예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
-ACAYKP 
-
-n = 6
-if a[n] == b[n]
-
-n = 5
-
-
 '''
 
 import sys
