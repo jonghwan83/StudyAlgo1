41c078b0ead0459f99258f406f23eaf04df2c345
jonghwan83 <jonghwan.park83@gmail.com>
2022-01-24 21:06:44 +0900

    solved

--- acmicpc/acmicpc02565.py
+++ acmicpc/acmicpc02565.py
@@ -3,7 +3,24 @@ start = time.time()
 
 import sys
 
+N = int(sys.stdin.readline())
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, sys.stdin.readline().split())))
 
+arr.sort()
+arr2 = []
+
+for i in range(N):
+    arr2.append(arr[i][1])
+
+dp = [1 for _ in range(N)]
+for i in range(1, len(arr2)):
+    for j in range(i):
+        if arr2[i] > arr2[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(N - max(dp))
 
 end = time.time()
 print('elapsed time: {}'.format(end - start))
