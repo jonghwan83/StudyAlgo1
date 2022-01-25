21357ec972d8e2143cece77a6229f71ab8582633
jonghwan83 <jonghwan.park83@gmail.com>
2022-01-25 23:02:22 +0900

    solved

--- acmicpc/acmicpc11399.cpp
+++ acmicpc/acmicpc11399.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
 
 int main(){
+
     int N;
+    scanf("%d", &N);
+
+    int arr[N];
+    for (int i=0; i < N; i++){
+        scanf("%d", &arr[i]);
+    }
+    sort(arr, arr + N);
+
+    int total = 0;
+    for (int i=0; i < N; i++) {
+        total = total + (N - i) * arr[i];
+    }
+    printf("%d\n", total);
 }
