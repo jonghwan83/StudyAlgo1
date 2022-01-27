21ce9c0cfb8b830d5e172cc4ea7bdfca073bf4b5
jonghwan83 <jonghwan.park83@gmail.com>
2022-01-27 21:55:12 +0900

    solved

--- acmicpc/acmicpc01931.py
+++ acmicpc/acmicpc01931.py
@@ -1,21 +1,41 @@
 import time
+# import random
+
+# N = 100000
+# meetings = []
+# for _ in range(N):
+#     a = random.randint(0, 2**31 - 1)
+#     b = random.randint(a, 2**31 - 1)
+#     meetings.append([a, b])
+
+start = time.time()
 
 import sys
 
 N = int(sys.stdin.readline())
-arr = []
+meetings = []
 for _ in range(N):
-    arr.append(list(map(int, sys.stdin.readline().split())))
-start = time.time()
+    meetings.append(list(map(int,sys.stdin.readline().split())))
 
-arr.sort()
-t = 0
-c = 0
-for i in range(len(arr)):
-    if t < arr[i][0]:
-        t += arr[i][1]
-        c += 1
-print(t, c)
+meetings.sort()
+intervals = []
+for lst in meetings:
+    intervals.append(lst[1] - lst[0])
+
+cum = [1] * N
+nTime = [0] * N
+nTime[0] = meetings[0][1]
+
+for i in range(1, N):
+    cum[i] = cum[i-1]
+    nTime[i] = nTime[i-1]
+    if meetings[i][0] >= nTime[i]:
+        cum[i] += 1
+        nTime[i] = meetings[i][1]
+    elif intervals[i] < intervals[i-1]:
+        nTime[i] = min(meetings[i][1], nTime[i])
+
+print(cum[-1])
 
 end = time.time()
 print('elapsed time: {}'.format(end - start))
