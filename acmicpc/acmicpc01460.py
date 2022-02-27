754433340fd933e517f40ec2ac258cd10729f51f
jonghwan83 <jonghwan.park83@gmail.com>
2022-02-27 22:51:26 +0900

    solved

--- acmicpc/acmicpc01460.py
+++ acmicpc/acmicpc01460.py
@@ -1,3 +1,13 @@
+'''
+time complexity of insert -> O(N)
+'''
+
+import time
+
+t0 = time.time()
+
+import sys
+
 class Node:
     def __init__(self, value):
         self.data = value
@@ -6,6 +16,7 @@ class Node:
 class DLinked:
     def __init__(self):
         self.head = None
+        self.length = 0
 
     def read(self, index):
         current_node = self.head
@@ -15,10 +26,14 @@ class DLinked:
             current_node = current_node.next
             current_index += 1
 
-        return current_node.data
+        try:
+            return current_node.data
+        except:
+            return None
 
     def insert(self, index, value):
         new_node = Node(value)
+        self.length += 1
 
         if self.head:
             if index == 0:
@@ -36,12 +51,47 @@ class DLinked:
         else:
             self.head = new_node
 
+    def delete(self, index):
+        self.length -= 1
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        current_node = self.head
+        current_index = 0
+        while current_index < index - 1:
+            current_node = current_node.next
+            current_index += 1
+        node_after_deleted_node = current_node.next.next
+        current_node.next = node_after_deleted_node
+
+
+txt = sys.stdin.readline().strip()
+
 linked = DLinked()
-linked.insert(0, 10)
-linked.insert(0, 5)
-linked.insert(1, 3)
-linked.insert(3, 7)
-print(linked.read(0))
-print(linked.read(1))
-print(linked.read(2))
-print(linked.read(3))
+for i, s in enumerate(txt):
+    linked.insert(i, s)
+cursor = i
+
+n = int(sys.stdin.readline())
+for _ in range(n):
+    cmd = sys.stdin.readline().strip()
+    if cmd[0] == 'L' and cursor > -1:
+        cursor -= 1
+    elif cmd[0] == 'D' and cursor < linked.length - 1:
+        cursor += 1
+    elif cmd[0] == 'P':
+        cursor += 1
+        linked.insert(cursor, cmd[2])
+    elif cmd[0] == 'B' and cursor > -1:
+        linked.delete(cursor)
+        cursor -= 1
+
+index = 0
+while linked.read(index):
+    print(linked.read(index), end='')
+    index += 1
+print()
+
+t1 = time.time()
+print('elapsed: {}'.format(t1- t0))
