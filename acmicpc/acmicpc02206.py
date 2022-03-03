81de77b17100a35108775f6b0b02a759a3b49756
jonghwan83 <jonghwan.park83@gmail.com>
2022-03-03 21:32:40 +0900

    solved

--- acmicpc/acmicpc02206.py
+++ acmicpc/acmicpc02206.py
@@ -1,77 +1,45 @@
-# not enough time complexity
-
-import time
-import random
-
-t0 = time.time()
-
-n, m = [70,70]
-maze = [[0 for _ in range(m)] for _ in range(n)]
-k = []
-for i in range(n):
-    for j in range(m):
-        maze[i][j] = random.randint(0, 1)
-        if maze[i][j] == 1:
-            k.append([i, j])
-
 import sys
 from collections import deque
-from copy import deepcopy
 
-# n, m = map(int, sys.stdin.readline().split())
-# maze = [[0 for _ in range(m)] for _ in range(n)]
 
-# k = []
-# for i in range(n):
-#     str = sys.stdin.readline().strip()
-#     for j, s in enumerate(str):
-#         maze[i][j] = int(s)
-#         if maze[i][j] == 1:
-#             k.append([i, j])
-
-def bfs(maze, i, j):
+def bfs(maze):
     drows = [-1, 1, 0, 0]
     dcols = [0, 0, -1, 1]
-    outlet = 0
-    for d in range(4):
-        di = i + drows[d]
-        dj = j + dcols[d]
-        if 0 <= di < n and 0 <= dj < m:
-            if maze[di][dj] == 0:
-                outlet += 1
 
-    if outlet < 2:
-        return -1
-
-    maze2 = deepcopy(maze)
-    maze2[i][j] = 0
-
-    solution = [[-1 for _ in range(m)]for _ in range(n)]
+    solution = [[[-1, -1] for _ in range(m)] for _ in range(n)]
     queue = deque()
-    queue.append([0, 0])
-    solution[0][0] = 1
+
+    queue.append([0, 0, 1])
+    solution[0][0][1] = 1
+
     while queue:
-        crow, ccol = queue.popleft()
+        crow, ccol, block = queue.popleft()
+
+        if crow == n-1 and ccol == m-1:
+            return solution[n-1][m-1][block]
+
         for d in range(4):
             drow = crow + drows[d]
             dcol = ccol + dcols[d]
+
             if 0 <= drow < n and 0 <= dcol < m:
-                if maze2[drow][dcol] == 0 and solution[drow][dcol] == -1:
-                    queue.append([drow, dcol])
-                    solution[drow][dcol] = solution[crow][ccol] + 1
-    
-    return solution[n-1][m-1]
+                if maze[drow][dcol] == 1 and block:
+                    solution[drow][dcol][block - 1] = solution[crow][ccol][block] + 1
+                    queue.append([drow, dcol, block - 1])
+                if maze[drow][dcol] == 0 and solution[drow][dcol][block] == -1:
+                    solution[drow][dcol][block] = solution[crow][ccol][block] + 1
+                    queue.append([drow, dcol, block])
 
-ans = sys.maxsize
-for i, j in k:
-    temp = bfs(maze, i, j)
-    if temp != -1:
-        ans = min(temp, ans)
+    return -1
 
-if ans != sys.maxsize:
-    print(ans)
-else:
-    print(-1)
 
-t1 = time.time()
-print('elapsed :{}'.format(t1 - t0))
+n, m = map(int, sys.stdin.readline().split())
+maze = [[0 for _ in range(m)] for _ in range(n)]
+
+for i in range(n):
+   str = sys.stdin.readline().strip()
+   for j, s in enumerate(str):
+       maze[i][j] = int(s)
+
+answer = bfs(maze)
+print(answer)
