9c7710dcfc01a381631dbc211224535106adef16
qkrwg <qkrwg12@gmail.com>
2022-06-16 22:39:43 +0900

    on going

---
+++ acmicpc/acmicpc01717.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#define MAX 10000001
+
+using namespace std;
+
+
+
+bool solve(int n, int m) {
+	vector<int> graph[MAX];
+
+	for (int i = 0; i < n+1; i++) {
+		graph[i].clear();
+	}
+	
+	return 0;
+}
+
+int main() {
+	freopen("sample.txt", "r", stdin);
+
+	int n, m;	
+	cin >> n >> m;
+
+	solve(n, m);
+
+	return 0;
+}
