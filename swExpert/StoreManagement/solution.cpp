42eb540ede6add951891de6e492ddf872429593f
qkrwg <qkrwg12@gmail.com>
2022-06-23 22:52:29 +0900

    prep for sw test

---
+++ swExpert/StoreManagement/solution.cpp
@@ -0,0 +1,22 @@
+
+
+
+void init() {
+	return;
+}
+
+int buy(int bId, int mProduct, int mPrice, int mQuantity) {
+	return 0;
+}
+
+int cancel(int bId) {
+	return 0;
+}
+
+int sell(int sId, int mProduct, int mPrice, int mQuantity) {
+	return 0;
+}
+
+int refund(int sId) {
+	return 0;
+}
