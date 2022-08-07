4721dd1f8b6ec7c801d0eaced09940dbf26a1f75
jonghwan83 <jonghwan.park83@gmail.com>
2022-08-07 23:59:42 +0900

    on going

--- swExpert/SurvivalTrain/solution.cpp
+++ swExpert/SurvivalTrain/solution.cpp
@@ -17,22 +17,38 @@ void init(int N, int M, int J, int mPoint[], int mJobID[])
 {
     passengers.clear();
     passengersByJob.clear();
+    minHeap.clear();
+    maxHeap.clear();
     
     nCars = N / M;
 
     for (int i = 0; i < N; i++)
     {
         int car = i / M;
-        vector<int> temp = { mPoint[i], mJobID[i], car, 0, 1 };
+        vector<int> temp;
+        temp.push_back(mPoint[i]);
+        temp.push_back(mJobID[i]);
+        temp.push_back(car);
+        temp.push_back(0);
+        temp.push_back(1);
+
         passengers.push_back(temp);
 
         passengersByJob[car].push_back(i);
         
-        vector<int> maxTemp = { mPoint[i], i, 0 };
+        vector<int> maxTemp;
+        maxTemp.push_back(mPoint[i]);
+        maxTemp.push_back(i);
+        maxTemp.push_back(0);
+
         maxHeap[car].push_back(maxTemp);
         push_heap(maxHeap[car].begin(), maxHeap[car].end());
 
-        vector<int> minTemp = { -mPoint[i], -i, 0 };
+        vector<int> minTemp;
+        minTemp.push_back(-mPoint[i]);
+        minTemp.push_back(-i);
+        minTemp.push_back(0);
+
         minHeap[car].push_back(minTemp);
         push_heap(minHeap[car].begin(), minHeap[car].end());
     }
@@ -50,11 +66,19 @@ int update(int mID, int mPoint)
     
     int car = passengers[mID][2];
 
-    vector<int> maxTemp = { passengers[mID][0], mID, passengers[mID][3] };
+    vector<int> maxTemp;
+    maxTemp.push_back(passengers[mID][0]);
+    maxTemp.push_back(mID);
+    maxTemp.push_back(passengers[mID][3]);
+
     maxHeap[car].push_back(maxTemp);
     push_heap(maxHeap[car].begin(), maxHeap[car].end());
 
-    vector<int> minTemp = { -passengers[mID][0], -mID, passengers[mID][3] };
+    vector<int> minTemp;
+    minTemp.push_back(-passengers[mID][0]);
+    minTemp.push_back(-mID);
+    minTemp.push_back(passengers[mID][3]);
+
     minHeap[car].push_back(minTemp);
     push_heap(minHeap[car].begin(), minHeap[car].end());
     
@@ -97,8 +121,12 @@ int move(int mNum)
                     if ((ver == passengers[pID][3]) && (passengers[pID][4]) && (passengers[pID][2] == c))
                     {
                         ans += point;
-                        vector<int> tempArr = { -point, -pID, ver };
-                        tempMin[c].push_back(tempArr);
+                        vector<int> minArr;
+                        minArr.push_back(-point);
+                        minArr.push_back(-pID);
+                        minArr.push_back(ver);
+
+                        tempMin[c].push_back(minArr);
                         passengers[pID][4] = 0;
 
                         pop_heap(minHeap[c].begin(), minHeap[c].end());
@@ -124,13 +152,16 @@ int move(int mNum)
                     if ((ver == passengers[pID][3]) && (passengers[pID][4]) && (passengers[pID][2] == c))
                     {
                         ans += point;
-                        vector<int> tempArr = { point, pID, ver };
-                        tempMax[c].push_back(tempArr);
+                        vector<int> maxArr;
+                        maxArr.push_back(point);
+                        maxArr.push_back(pID);
+                        maxArr.push_back(ver);
+
+                        tempMax[c].push_back(maxArr);
                         passengers[pID][4] = 0;
 
                         pop_heap(maxHeap[c].begin(), maxHeap[c].end());
                         maxHeap[c].pop_back();
-                        cout << "break" << endl;
                         break;
                     }
                     else
@@ -155,11 +186,17 @@ int move(int mNum)
                 
                 passengers[pID][2] = c + 1;
 
-                vector<int> tempArr = { -point, -pID, ver };
+                vector<int> tempArr;
+                tempArr.push_back(-point);
+                tempArr.push_back(-pID);
+                tempArr.push_back(ver);
+
                 minHeap[c+1].push_back(tempArr);
                 push_heap(minHeap[c].begin(), minHeap[c].end());
 
-                tempArr = { point, pID, ver };
+                tempArr[0] = point;
+                tempArr[1] = pID;
+
                 maxHeap[c+1].push_back(tempArr);
                 push_heap(maxHeap[c].begin(), maxHeap[c].end());
 
@@ -179,11 +216,17 @@ int move(int mNum)
 
                 passengers[pID][2] = c - 1;
 
-                vector<int> tempArr = { -point, -pID, ver };
+                vector<int> tempArr;
+                tempArr.push_back(-point);
+                tempArr.push_back(-pID);
+                tempArr.push_back(ver);
+
                 minHeap[c-1].push_back(tempArr);
                 push_heap(minHeap[c].begin(), minHeap[c].end());
 
-                tempArr = { point, pID, ver };
+                tempArr[0] = point;
+                tempArr[1] = pID;
+
                 maxHeap[c-1].push_back(tempArr);
                 push_heap(maxHeap[c].begin(), minHeap[c].end());
 
@@ -194,6 +237,6 @@ int move(int mNum)
         }
     }
 
-    cout << ans << endl;
+    // cout << ans << endl;
     return ans;
 }
