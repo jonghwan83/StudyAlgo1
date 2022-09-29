33279684a80ba5507f7dc7fdc23fb7fc826c9398
jonghwan83 <jonghwan.park83@gmail.com>
2022-09-29 22:32:09 +0900

    on going

--- swExpert/ArrangeSeat/solution.py
+++ swExpert/ArrangeSeat/solution.py
@@ -1,26 +1,27 @@
+# need hash map for speed up
+
 drows = [0, 1, 1, 1, 0, -1, -1, -1]
 dcols = [1, 1, 0, -1, -1, -1, 0, 1]
 
 def seat2grid(mSeatNum):
     global size
 
-    r = mSeatNum // size[0]
-    c = mSeatNum % size[1] - 1
-    if c == -1:
-        c = 0
+    r = (mSeatNum - 1) // size[1]
+    c = (mSeatNum - 1) % size[1]
     
     return [r, c]
 
 def grid2seat(loc):
     global size
-    return loc[0] * size[0] + loc[1] + 1
+    return loc[0] * size[1] + loc[1] + 1
 
 def assignSeat(mSeatNum, k):
-    global seat, size
+    global seat, size, layerSeat
     r, c = seat2grid(mSeatNum)
 
     if seat[r][c] == 0:
         seat[r][c] = 1
+        layerSeat[mSeatNum] = k
         return grid2seat([r, c])
 
     if k >= max(size):
@@ -35,6 +36,7 @@ def assignSeat(mSeatNum, k):
             if (0 <= dr < size[0]) and (0 <= dc < size[1]):
                 if seat[dr][dc] == 0:
                     seat[dr][dc] = 1
+                    layerSeat[mSeatNum] = k
                     return grid2seat([dr, dc])
 
         k += 1
@@ -42,23 +44,25 @@ def assignSeat(mSeatNum, k):
     return 0
 
 def init(W, H):
-    global seat, alternatives, size
+    global seat, alternatives, size, unavailable, layerSeat
 
     seat = [[0] * W for _ in range(H)]    # 0: empty 1: occupied
-    alternatives = {}   # key: seat No., value: alternative seat No.
     size = [H, W]
-
-    # print(size)
-    # for lst in seat:
-    #     print(lst)
+    unavailable = [0] * (W * H + 1)   # index: seat No.   1: return 0
+    layerSeat = [1] * (W * H + 1)   # index: seat No. element: k layers
 
     return
 
 def selectSeat(mSeatNum):
-    result = assignSeat(mSeatNum, 1)
-    
-    # print('==seat==')
-    # for lst in seat:
-    #     print(lst)
+    global unavailable, layerSeat
+
+    if unavailable[mSeatNum]:
+        return 0
+
+    k = layerSeat[mSeatNum]
+    result = assignSeat(mSeatNum, k)
+    # store not available seat
+    if result == 0:
+        unavailable[mSeatNum] = 1
 
     return result
