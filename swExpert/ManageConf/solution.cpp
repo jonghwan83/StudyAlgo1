75c5b2072ce89cf555f7448a7110a58df524a65f
jonghwan83 <jonghwan.park83@gmail.com>
2022-10-05 20:34:24 +0900

    solving

--- swExpert/ManageConf/solution.cpp
+++ swExpert/ManageConf/solution.cpp
@@ -10,19 +10,19 @@ using namespace std;
 
 struct FileDetail {
 public:
-	int cTime;
-	int eTime;
-	string fname;
-	string data;
+    int cTime;
+    int eTime;
+    string fname;
+    string data;
     bool isExist;
 
-	FileDetail(int ctime, int etime, string filename, string content) {
-		cTime = ctime;
-		eTime = etime;
-		fname = filename;
-		data = content;
+    FileDetail(int ctime, int etime, string filename, string content) {
+        cTime = ctime;
+        eTime = etime;
+        fname = filename;
+        data = content;
         isExist = true;
-	}
+    }
 };
 
 bool compare(pair<int, int> a, pair<int, int> b) { return a.first < b.first ? false : true; }
@@ -35,19 +35,19 @@ unordered_map<string, string> hashParent;
 
 void init() {
     pQueue.clear();
-	project.clear();
-	dict.clear();
-	projectTree.clear();
-	hashParent.clear();
-	
-	vector<string> temp = {};
-	projectTree["root"] = temp;
+    project.clear();
+    dict.clear();
+    projectTree.clear();
+    hashParent.clear();
 
-	return;
+    vector<string> temp = {};
+    projectTree["root"] = temp;
+
+    return;
 }
 
 void create(int mTime, char mBranch[], char mFile[], char mData[]) {
-	FileDetail temp(mTime, mTime, mFile, mData);
+    FileDetail temp(mTime, mTime, mFile, mData);
 
     if (pQueue[mBranch].size() >= 50) {
         int idxFront = pQueue[mBranch][0].second;
@@ -57,37 +57,42 @@ void create(int mTime, char mBranch[], char mFile[], char mData[]) {
         pQueue[mBranch].pop_back();
     }
 
-	project[mBranch].push_back(temp);
+    project[mBranch].push_back(temp);
     pQueue[mBranch].push_back(make_pair(mTime, (int)project[mBranch].size() - 1));
     push_heap(pQueue[mBranch].begin(), pQueue[mBranch].end(), compare);
     dict[mBranch][mFile] = (int)project[mBranch].size() - 1;
 
-	return;
+    return;
 }
 
 void createFromChild(int mTime, string mBranch, string mFile, string mData, int eTime) {
     FileDetail temp(mTime, eTime, mFile, mData);
 
-    if (pQueue[mBranch].size() >= 50) {
-        int idxFront = pQueue[mBranch][0].second;
+    project[mBranch].push_back(temp);
+    pQueue[mBranch].push_back(make_pair(mTime, (int)project[mBranch].size() - 1));
+    push_heap(pQueue[mBranch].begin(), pQueue[mBranch].end(), compare);
+    dict[mBranch][mFile] = (int)project[mBranch].size() - 1;
+
+    return;
+}
+
+void checkBranch(string mBranch) {
+    // check if branck size > 50
+    int idxFront;
+    while (pQueue[mBranch].size() > 50) {
+        idxFront = pQueue[mBranch][0].second;
         project[mBranch][idxFront].isExist = false;
 
         pop_heap(pQueue[mBranch].begin(), pQueue[mBranch].end(), compare);
         pQueue[mBranch].pop_back();
     }
-
-	project[mBranch].push_back(temp);
-    pQueue[mBranch].push_back(make_pair(mTime, (int)project[mBranch].size() - 1));
-    push_heap(pQueue[mBranch].begin(), pQueue[mBranch].end(), compare);
-    dict[mBranch][mFile] = (int)project[mBranch].size() - 1;
-
-	return;
+    return;
 }
 
 void copyFile(string mChild, string mFile, string mData, int cTime, int eTime) {
     string parent = hashParent[mChild];
     int idx = dict[parent][mFile];
-    
+
     if ((project[parent][idx].isExist) && (project[parent][idx].fname == mFile)) {
         if (cTime == project[parent][idx].cTime) {
             if (eTime > project[parent][idx].eTime) {
@@ -99,70 +104,77 @@ void copyFile(string mChild, string mFile, string mData, int cTime, int eTime) {
     else {
         createFromChild(cTime, parent, mFile, mData, eTime);
     }
-	return;
+    return;
 }
 
 void mergeToParent(string mBranch) {
-    
-    for (int i=0; i < project[mBranch].size(); i++) {
-        copyFile(mBranch, project[mBranch][i].fname, project[mBranch][i].data, project[mBranch][i].cTime, project[mBranch][i].eTime);
+
+    int idx;
+    for (int i = 0; i < pQueue[mBranch].size(); i++) {
+        idx = pQueue[mBranch][i].second;
+        copyFile(mBranch, project[mBranch][idx].fname, project[mBranch][idx].data, project[mBranch][idx].cTime, project[mBranch][i].eTime);
     }
+
+    // check parent node size
+    string parent = hashParent[mBranch];
+    checkBranch(parent);
 
     pQueue[mBranch].clear();
     project[mBranch].clear();
     dict[mBranch].clear();
     projectTree[mBranch].clear();
-    
+
     return;
 }
 
 void callTree(string mBranch) {
 
-	if (projectTree[mBranch].size() == 0) {
-		mergeToParent(mBranch);
-		return;
-	}
-	
-	for (int i = 0; i < projectTree[mBranch].size(); i++) {
-		callTree(projectTree[mBranch][i]);
-	}
+    if (projectTree[mBranch].size() == 0) {
+        mergeToParent(mBranch);
+        return;
+    }
 
-	mergeToParent(mBranch);
-	return;
+    for (int i = 0; i < projectTree[mBranch].size(); i++) {
+        callTree(projectTree[mBranch][i]);
+    }
+
+    mergeToParent(mBranch);
+    return;
 }
 
 void edit(int mTime, char mBranch[], char mFile[], char mData[]) {
-	int idx = dict[mBranch][mFile];
+    int idx = dict[mBranch][mFile];
     project[mBranch][idx].data = mData;
     project[mBranch][idx].eTime = mTime;
 
-	return;
+    return;
 }
 
 void branch(int mTime, char mBranch[], char mChild[]) {
-	project[mChild] = project[mBranch];
+    project[mChild] = project[mBranch];
     pQueue[mChild] = pQueue[mBranch];
-	dict[mChild] = dict[mBranch];
+    dict[mChild] = dict[mBranch];
 
-	projectTree[mBranch].push_back(mChild);
-	hashParent[mChild] = mBranch;
+    projectTree[mBranch].push_back(mChild);
+    hashParent[mChild] = mBranch;
 
     return;
 }
 
 void merge(int mTime, char mBranch[]) {
-	callTree(mBranch);
+    callTree(mBranch);
 }
 
 int readfile(int mTime, char mBranch[], char mFile[], char retString[]) {
-	int lenString = 0;
+    int lenString = 0;
     int idx = dict[mBranch][mFile];
     string data = project[mBranch][idx].data;
-    for (int i=0; i < data.size(); i++) {
+
+    for (int i = 0; i < data.size(); i++) {
         lenString++;
         retString[i] = data[i];
     }
     retString[data.size()] = '\0';
 
-	return lenString;
+    return lenString;
 }
