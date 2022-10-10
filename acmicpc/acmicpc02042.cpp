50de0838a1996260cedae2cc58b6c026e0bd2507
jonghwan83 <jonghwan.park83@gmail.com>
2022-10-10 23:57:04 +0900

    on going

--- acmicpc/acmicpc02042.cpp
+++ acmicpc/acmicpc02042.cpp
@@ -13,44 +13,59 @@ void makeTree(int idx, int start, int end) {
         return;
     }
 
-    makeTree(2 * idx + 1, start, (start + end) / 2);
-    makeTree(2 * idx + 2, (start + end) / 2 + 1, end);
-    tree[idx] = tree[2 * idx + 1] + tree[2 * idx + 2];
+    makeTree(2 * idx, start, (start + end) / 2);
+    makeTree(2 * idx + 1, (start + end) / 2 + 1, end);
+    tree[idx] = tree[2 * idx] + tree[2 * idx + 1];
 }
 
 void update(int node, int idx, int start, int end, int diff) {
     if (idx < start || idx > end) { return; }
     tree[node] = tree[node] + diff;
     if (start != end) {
-        update(2 * node + 1, idx, start, (start + end) / 2, diff);
-        update(2 * node + 2, idx, (start + end) / 2 + 1, end, diff);
+        update(2 * node, idx, start, (start + end) / 2, diff);
+        update(2 * node + 1, idx, (start + end) / 2 + 1, end, diff);
     }
 }
 
-void updateTree(int idx, int start, int end, int val) {
-    int diff = arr[idx] - val;
+void updateTree(int idx, int val) {
+    int diff = val - arr[idx];
     arr[idx] = val;
-    update(0, idx, start, end, diff);
+    update(1, idx, 0, arr.size() - 1, diff);
+}
+
+int query(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) { return 0; }
+    if (left <= start && end <= right) { return tree[node]; }
+    int lsum = query(node * 2, start, (start + end) / 2, left, right);
+    int rsum = query(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+    
+    return lsum + rsum;
 }
 
 int main() {
-    freopen("sample_input.txt", "r", stdin);
+    freopen("../sample_input.txt", "r", stdin);
     cin >> n >> m >> k;
 
     arr.resize(n);
-    tree.resize(2 * n - 1);
-    for (int i=0; i < n; i++) {
+    tree.resize(2 * n);
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    makeTree(0, 0, arr.size()-1);
+    makeTree(1, 0, arr.size()-1);
     int c, idx, val;
     for (int i=0; i < n+m; i++) {
         cin >> c >> idx >> val;
         if (c == 1) {
-            updateTree(idx, 0, tree.size() - 1,val);
+            updateTree(idx - 1, val);
+        }
+        if (c == 2) {
+            cout << query(1, 0, arr.size()-1, idx, val) << endl;
         }
     }
 
-    cout << tree[0] << endl;
+    for (int i=0; i < tree.size(); i++) {
+        cout << tree[i] << " ";
+    }
+    cout << endl;
 }
