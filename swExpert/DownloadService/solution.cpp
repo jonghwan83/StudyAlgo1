e9c3e23bded6fe49f42c68011929883ff6cc4e22
Jonghwan Park <83850068+jonghwan83@users.noreply.github.com>
2022-10-19 18:33:29 +0900

    Update solution.cpp

--- swExpert/DownloadService/solution.cpp
+++ swExpert/DownloadService/solution.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -10,7 +11,6 @@ struct Result {
 class PC {  
 public: 
     int id;
-    int speed;
     int capacity;
     int eTime;
 };
@@ -25,39 +25,60 @@ public:
     int sTime;
 };
 
-vector<PC> downloads;   // pc id 로 정보를 검색
+vector<PC> downloads;
 vector<Clients> serverTree;
 vector<int> idxParent;
+int currentTime;
 
 void updateConnection(int hubID) {
     int parent = idxParent[hubID];
     if (serverTree[hubID].isPC) {
         serverTree[parent].nConnected++;
-    } 
+    }
     else {
         if (serverTree[hubID].nConnected == 1) {
-            serverTree[parent].nConnected++;
-        }
+        serverTree[parent].nConnected++;
+        } 
+        else { return; }    
     }
+    
     if (parent == 0) { return; }
     updateConnection(parent);
 }
 
+void executeDownload(int pcID) {
+    downloads[pcID].capacity -= serverTree[pcID].speed * (currentTime - serverTree[pcID].sTime);
+    if (downloads[pcID].capacity <= 0 ) {
+        downloads[pcID].eTime = currentTime + (downloads[pcID].capacity / serverTree[pcID].sTime);
+    }
+    serverTree[pcID].sTime = currentTime;
+}
+
 void updateSpeed(int hubID) {
     // from root node
-
     for (int idx: serverTree[hubID].children) {
-        if (serverTree[idx].nConnected == 0) { continue; }
-        serverTree[idx].speed = serverTree[hubID].speed / serverTree[hubID].nConnected;   
         if (serverTree[idx].isPC) {
-            downloads[idx].speed = serverTree[idx].speed;
-        } 
+            executeDownload(idx);
+            serverTree[idx].speed = serverTree[hubID].speed / serverTree[hubID].nConnected;
+        }
+        if (serverTree[idx].nConnected == 0) { continue; }
+        serverTree[idx].speed = serverTree[hubID].speed / serverTree[hubID].nConnected;
         updateSpeed(idx);
+    }
+}
+
+void updateHub(int hubID) {
+    if (serverTree[hubID].children.empty()) { return; }
+    for (int idx: serverTree[hubID].children) {
+        if (serverTree[idx].isPC) { executeDownload(idx); }
+        updateHub(idx);
     }
 }
 
 void init(int mCapa)
 {
+    currentTime = 0;
+
     downloads.clear();
     serverTree.clear();
     idxParent.clear();
@@ -73,6 +94,7 @@ void init(int mCapa)
 
 void addHub(int mTime, int mParentID, int mID)
 {
+    currentTime = mTime;
     serverTree[mParentID].children.push_back(mID);
     serverTree[mID].cID = mID;
     serverTree[mID].sTime = mTime;
@@ -82,19 +104,27 @@ void addHub(int mTime, int mParentID, int mID)
 
 int removeHub(int mTime, int mID)
 {
+    currentTime = mTime;
+    updateHub(mID);
     return -1;
 }
 
 void requestDL(int mTime, int mParentID, int mpcID, int mSize)
 {
+    currentTime = mTime;
     serverTree[mParentID].children.push_back(mpcID);
     serverTree[mpcID].cID = mpcID;
     serverTree[mpcID].isPC = true;
     serverTree[mpcID].sTime = mTime;
 
     idxParent[mpcID] = mParentID;
+
+    downloads[mpcID].capacity = mSize;
+
     updateConnection(mpcID);
     updateSpeed(0);
+
+    // printf("speed: %d %d %d\n", serverTree[50].speed, serverTree[0].nConnected, downloads[50].capacity);
 }
 
 Result checkPC(int mTime, int mpcID)
