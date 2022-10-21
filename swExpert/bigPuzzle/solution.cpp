af958660c024d572ba5a0b424b3d88172ed2fb7a
jonghwan83 <jonghwan.park83@gmail.com>
2022-10-21 23:33:27 +0900

    on going

--- swExpert/bigPuzzle/solution.cpp
+++ swExpert/bigPuzzle/solution.cpp
@@ -1,20 +1,52 @@
 #include <vector>
-
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
-void init(int N, int M, int mU[][4], int mR[][4], int mB[][4], int mL[][4])
-{
+class Piece {
+public:
+    int up;
+    int right;
+    int down;
+    int left;
+};
+
+vector< vector<Piece> > board;
+unordered_map<int, int> possiblePiece;
+
+int getReverse(int p) {
+    string s = to_string(p);
+    string k;
+    for (int i=s.size() - 1; i > -1; i--) {
+        k += s[i];
+    }
+    printf("%s\n", k.c_str());
+    return stoi(k);
+}
+
+
+void init(int N, int M, int mU[][4], int mR[][4], int mB[][4], int mL[][4]) {
+    board.clear();
+    possiblePiece.clear();
+
+    board.resize(N + 1);
+    for (int i=0; i <= N; i++) {
+        board[i].resize(N + 1);
+    }
+
+    for (int i=0; i < N; i++) {
+        for (int j=0; j < 4; j++) {
+            
+        }
+    }
+}
+
+void destroy() {
 
 }
 
-void destroy()
-{
-
-}
-
-int put(int mPiece[4])
-{
+int put(int mPiece[4]) {
     return -1;
 }
 
