12577885a981073a5b18684bf24e551f1b6cc3a0
Jonghwan Park <83850068+jonghwan83@users.noreply.github.com>
2023-01-19 20:05:24 +0900

    solved

--- swExpert/Civilization/solution.py
+++ swExpert/Civilization/solution.py
@@ -1,106 +1,168 @@
+class Node:
+    def __init__(self):
+        self.row = 0
+        self.col = 0
+        self.next = None
 
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        self.id = 0
+        self.isRemoved = False
+
+    def append(self, r, c):
+        current_node = Node()
+        current_node.row = r
+        current_node.col = c
+        current_node.next = self.head
+
+        if (not self.tail):
+            self.tail = current_node
+
+        self.head = current_node
+        self.length += 1
+
+MAXCIVLIZATION = 60001
 drows = [-1, 1, 0, 0]
 dcols = [0, 0, -1, 1]
 
-class Civil:
-    def __init__(self, N) -> None:
-        self.grid = [[0] * N for _ in range(N)]
-        self.N = N
-        self.territory = {}
+def assembleLink(idx1, idx2):
+    global civilization
+
+    civilization[idx1].tail.next = civilization[idx2].head
+
+    node = civilization[idx2].head
+    while node:
+        grid[node.row][node.col] = idx1
+        node = node.next
+
+    civilization[idx1].tail = civilization[idx2].tail
+    civilization[idx1].length += civilization[idx2].length
+
+    civilization[idx2].isRemoved = True
+    civilization[idx2].length = 0
 
 
-def init(N):
-    global civil
+def checkNeighbor(r, c):
+    global grid, civilization
 
-    civil = Civil(N)
-    return
+    ans = -1
 
-def newCivilization(r, c, mID):
-    global civil
-
-    r -= 1
-    c -= 1
-
-    civil.grid[r][c] = mID
-    if civil.territory.get(mID):
-        civil.territory[mID].append([r, c])
-    else:
-        civil.territory[mID] = [[r, c]]
-
-    edges = {}
+    neighbors = {}
     for i in range(4):
         dr = r + drows[i]
         dc = c + dcols[i]
 
-        if 0 <= dr < civil.N and 0 <= dc < civil.N:
-            if civil.grid[dr][dc] != 0:
-                if edges.get(civil.grid[dr][dc]):
-                    edges[civil.grid[dr][dc]] += 1
+        if 0 < dr < n_grid and 0 < dc < n_grid:
+            if grid[dr][dc] > -1 and not civilization[grid[dr][dc]].isRemoved:
+                if neighbors.get(grid[dr][dc]):
+                    neighbors[grid[dr][dc]] += 1
                 else:
-                    edges[civil.grid[dr][dc]] = 1
+                    neighbors[grid[dr][dc]] = 1
 
-    if bool(edges):
-        base = 0
-        surrounded = 1000000001
-        for key, value in edges.items():
-            if value > base:
-                base = value
-                surrounded = key
-            elif value == base:
-                surrounded = min(key, surrounded)
+    cnt = 0
+    for key, val in neighbors.items():
+        if val > cnt:
+            ans = key
+            cnt = val
+        elif val == cnt:
+            if civilization[ans].id > civilization[key].id:
+                ans = key
 
-        civil.grid[r][c] = surrounded
-        civil.territory[surrounded].append([r, c])
-        civil.territory[mID].clear()
-
-        return surrounded
-
-    return mID
-
-def removeCivilization(mID):
-    global civil
-
-    if civil.territory.get(mID):
-        if len(civil.territory[mID]) == 0:
-            return 0
-        else:
-            pass
-    else:
-        return 0
-
-    ans = 0
-    for r, c, in civil.territory[mID]:
-        civil.grid[r][c] = 0
-        ans += 1
-
-    civil.territory[mID].clear()
     return ans
 
-def getCivilization(r, c):
-    global civil
-    
-    r -= 1
-    c -= 1
 
-    return civil.grid[r][c] 
+def init(N):
+    global civilization, hashCivil, grid, n_grid, cIdx
 
-def getCivilizationArea(mID):
-    global civil
+    if (N * N + 1 <= MAXCIVLIZATION):
+        civilization = [LinkedList() for _ in range(N * N + 1)]
+    else:
+        civilization = [LinkedList() for _ in range(MAXCIVLIZATION)]
 
-    if civil.territory.get(mID):
-        pass
+    cIdx = 1
+    hashCivil = {}
+
+    grid = [[-1 for _ in range(N + 1)] for _ in range(N + 1)]
+    n_grid = N + 1
+
+    return
+
+
+def newCivilization(r, c, mID):
+    global civilization, hashCivil, grid, cIdx
+
+    neighbor = checkNeighbor(r, c)
+    if (neighbor > 0):
+        civilization[neighbor].append(r, c)
+
+        grid[r][c] = neighbor
+    else:
+        civilization[cIdx].id = mID
+        civilization[cIdx].append(r, c)
+
+        grid[r][c] = cIdx
+        hashCivil[mID] = cIdx
+
+        cIdx += 1
+
+    return civilization[grid[r][c]].id
+
+
+def removeCivilization(mID):
+    global civilization, hashCivil
+
+    if hashCivil.get(mID):
+        idx = hashCivil[mID]
     else:
         return 0
 
-    return len(civil.territory[mID])
+    if civilization[idx].isRemoved:
+        return 0
+
+    ans = civilization[idx].length
+    civilization[idx].isRemoved = True
+
+    return ans
+
+
+def getCivilization(r, c):
+    global civilization, grid
+
+    idx = grid[r][c]
+
+    if idx == -1:
+        return 0
+
+    if civilization[idx].isRemoved:
+        return 0
+
+    return civilization[idx].id
+
+
+def getCivilizationArea(mID):
+    global civilization, hashCivil
+
+    if hashCivil.get(mID):
+        idx = hashCivil[mID]
+    else:
+        return 0
+
+    if civilization[idx].isRemoved:
+        return 0
+
+    return civilization[idx].length
+
 
 def mergeCivilization(mID1, mID2):
-    global civil
-    
-    for r, c in civil.territory[mID2]:
-        civil.grid[r][c] = mID1
-    
-    civil.territory[mID1] += civil.territory[mID2]
-    civil.territory[mID2].clear()
+    global civilization, hashCivil
 
-    return len(civil.territory[mID1])
+    idx1 = hashCivil[mID1]
+    idx2 = hashCivil[mID2]
+
+    assembleLink(idx1, idx2)
+
+    return civilization[idx1].length
