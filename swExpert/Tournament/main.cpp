c088c149b4aae1aa7d7c0fa534775b5d2ca604b1
jonghwan83 <jonghwan.park83@gmail.com>
2023-05-09 22:31:41 +0900

    solved

---
+++ swExpert/Tournament/main.cpp
@@ -0,0 +1,157 @@
+﻿#ifndef _CRT_SECURE_NO_WARNINGS
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#endif
+
+
+
+#include<stdio.h>
+#include<time.h>
+
+
+
+#define MAX_N 131072
+
+#define ABS(a) (((a) < 0) ? -(a) : (a))
+
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
+
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
+
+extern void modifyTournament(int N, int mOrder[]);
+
+static int org[MAX_N];
+
+static int winners[MAX_N / 2];
+
+static int losers[MAX_N / 2];
+
+static int copied[MAX_N];
+
+static int N;
+
+static bool ok;
+
+void swapOrder(int mIndexA, int mIndexB)
+
+{
+
+	if (!ok || mIndexA < 0 || mIndexA >= N || mIndexB < 0 || mIndexB >= N)
+
+	{
+
+		ok = false;
+
+		return;
+
+	}
+
+	int tmp = org[mIndexA];
+
+	org[mIndexA] = org[mIndexB];
+
+	org[mIndexB] = tmp;
+
+}
+
+
+
+static bool isRightGroup(int s, int e) {
+
+	if (e - s <= 1)
+
+		return true;
+
+	int mid = (e + s) / 2;
+
+	for (int i = 0; i < mid - s; i++)
+
+	{
+
+		winners[i] = MIN(org[s + 2 * i], org[s + 2 * i + 1]);
+
+		losers[i] = MAX(org[s + 2 * i], org[s + 2 * i + 1]);
+
+		if (winners[i] > mid || losers[i] <= mid)
+
+			return false;
+
+	}
+
+	for (int i = 0; i < mid - s; i++)
+
+	{
+
+		org[s + i] = winners[i];
+
+		org[mid + i] = losers[i];
+
+	}
+
+	return isRightGroup(s, mid) && isRightGroup(mid, e);
+
+}
+
+
+
+static bool run()
+
+{
+
+	ok = true;
+
+	scanf("%d", &N);
+
+
+
+	for (int i = 0; i < N; i++)
+
+	{
+
+		scanf("%d", &org[i]);
+
+		copied[i] = org[i];
+
+	}
+
+	modifyTournament(N, copied);
+
+	return ok && isRightGroup(0, N);
+
+}
+
+
+
+int main()
+
+{
+	clock_t start, end;
+	start = clock();
+
+	setbuf(stdout, NULL);
+
+	freopen("sample_input.txt", "r", stdin);
+
+	int T, MARK;
+
+	scanf("%d%d", &T, &MARK);
+
+
+
+	for (int tc = 1; tc <= T; tc++)
+
+	{
+
+		int score = run() ? MARK : 0;
+
+		printf("#%d %d\n", tc, score);
+
+	}
+
+	end = clock() - start;
+	printf("elapsed: %f\n", (float)end / CLOCKS_PER_SEC);
+
+	return 0;
+
+}
---
+++ swExpert/Tournament/solution.cpp
@@ -0,0 +1,72 @@
+extern void swapOrder(int mIndexA, int mIndexB);
+
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+#define MAXNUM 131072
+
+int answer[MAXNUM];
+int arr[MAXNUM];
+int hashIdx[MAXNUM + 1];
+
+void merge(int arr[], int left, int right) {
+    int n = right - left + 1;
+    int mid = (right + left) / 2;
+
+    int temp[n];
+    int i = 0;
+    while (i < n) {
+        if (i % 2 == 0) {
+            temp[i] = arr[left + i / 2];
+        }
+        else {
+            temp[i] = arr[mid + 1 + i / 2];
+        }
+        i++;
+    }
+
+    for (int i = left; i <= right; i++) {
+        arr[i] = temp[i - left];
+    }
+}
+
+void merge_sort(int arr[], int left, int right) {
+    int mid;
+
+    if (left < right) {
+        mid = (left + right) / 2;
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        merge(arr, left, right);
+    }
+}
+
+void modifyTournament(int N, int mOrder[])
+{
+    bool checked[MAXNUM];
+    for (int i = 0; i < N; i++) {
+        arr[i] = mOrder[i];
+        hashIdx[mOrder[i]] = i;
+        answer[i] = i + 1;
+        checked[i] = false;
+    }
+
+    merge_sort(answer, 0, N - 1);
+
+    for (int i = 0; i < N; i++) {
+        if (arr[i] != answer[i]) {
+            int old = arr[i];
+            arr[i] = arr[hashIdx[answer[i]]];
+            arr[hashIdx[answer[i]]] = old;
+
+            swapOrder(i, hashIdx[answer[i]]);
+
+            int temp = hashIdx[old];
+            hashIdx[old] = hashIdx[answer[i]];
+            hashIdx[answer[i]] = temp;
+
+        }
+    }
+}
