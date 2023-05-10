6a9139daaa0c858805a79f057ddc803ed7255ac6
jonghwan83 <jonghwan.park83@gmail.com>
2023-05-11 01:06:54 +0900

    solved

--- swExpert/WordProcessor/main.cpp
+++ swExpert/WordProcessor/main.cpp
@@ -1,4 +1,4 @@
-	
+
 #ifndef _CRT_SECURE_NO_WARNINGS
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -49,90 +49,90 @@ static int run()
 
 {
 
-int len;
+    int len;
 
-scanf("%d%s", &len, buf_b1);
-
-
-
-init(len, buf_b1);
-
-int cmdCount;
-
-scanf("%d", &cmdCount);
-
-
-int ret_val = 1;
-
-int ftype, start, end, ret, ans;
-
-for (int i = 0; i < cmdCount; i++)
-
-{
-
-scanf("%d", &ftype);
-
-switch (ftype)
-
-{
-
-case ADD_WORD:
-
-scanf("%s", buf_s);
-
-addWord(buf_s);
-
-break;
-
-case REMOVE_WORD:
-
-scanf("%s", buf_s);
-
-removeWord(buf_s);
-
-break;
-
-case CORRECT:
-
-scanf("%d %d", &start, &end);
-
-ret = correct(start, end);
-
-scanf("%d", &ans);
-
-if (ret != ans)
-
-ret_val = 0;
-
-break;
-
-}
-
-}
-
-for (int i = 0; i < len; i++)
-
-buf_b2[i] = '\0';
-
-destroy(buf_b2);
+    scanf("%d%s", &len, buf_b1);
 
 
 
-scanf("%s", buf_b1);
+    init(len, buf_b1);
 
-for (int i = 0; i < len; i++)
+    int cmdCount;
 
-{
-
-if (buf_b1[i] != buf_b2[i])
-
-ret_val = 0;
-
-}
+    scanf("%d", &cmdCount);
 
 
+    int ret_val = 1;
 
-return ret_val;
+    int ftype, start, end, ret, ans;
+
+    for (int i = 0; i < cmdCount; i++)
+
+    {
+
+        scanf("%d", &ftype);
+
+        switch (ftype)
+
+        {
+
+            case ADD_WORD:
+
+                scanf("%s", buf_s);
+
+                addWord(buf_s);
+
+                break;
+
+            case REMOVE_WORD:
+
+                scanf("%s", buf_s);
+
+                removeWord(buf_s);
+
+                break;
+
+            case CORRECT:
+
+                scanf("%d %d", &start, &end);
+
+                ret = correct(start, end);
+
+                scanf("%d", &ans);
+
+                if (ret != ans)
+
+                    ret_val = 0;
+
+                break;
+
+        }
+
+    }
+
+    for (int i = 0; i < len; i++)
+
+        buf_b2[i] = '\0';
+
+    destroy(buf_b2);
+
+
+
+    scanf("%s", buf_b1);
+
+    for (int i = 0; i < len; i++)
+
+    {
+
+        if (buf_b1[i] != buf_b2[i])
+
+            ret_val = 0;
+
+    }
+
+
+
+    return ret_val;
 
 }
 
@@ -143,34 +143,34 @@ int main()
     clock_t start, end;
     start = clock();
 
-setbuf(stdout, NULL);
+    setbuf(stdout, NULL);
 
-freopen("sample_input.txt", "r", stdin);
-
-
-int T, MARK;
-
-scanf("%d %d", &T, &MARK);
+    freopen("sample_input.txt", "r", stdin);
 
 
+    int T, MARK;
 
-for (int tc = 1; tc <= T; tc++)
+    scanf("%d %d", &T, &MARK);
 
-{
 
-if (run() == 1)
 
-printf("#%d %d\n", tc, MARK);
+    for (int tc = 1; tc <= T; tc++)
 
-else
+    {
 
-printf("#%d 0\n", tc);
+        if (run() == 1)
 
-}
+            printf("#%d %d\n", tc, MARK);
 
-end = clock() - start;
-printf("elapsed: %f\n", (float) end / CLOCKS_PER_SEC);
+        else
 
-return 0;
+            printf("#%d 0\n", tc);
+
+    }
+
+    end = clock() - start;
+    printf("elapsed: %f\n", (float) end / CLOCKS_PER_SEC);
+
+    return 0;
 
 }
--- swExpert/WordProcessor/solution.cpp
+++ swExpert/WordProcessor/solution.cpp
@@ -2,173 +2,267 @@
 #include <string>
 #include <unordered_map>
 
-#define MAXDICT 2000
-#define MAXWORD 40000
+#define ALPHABET 26
+#define MAXWORD 1000
 #define MAXCHAR 11
 
-using namespace std;
-
-class Dictionary {
+class HeapNode {
 public:
-	string word;
-	bool isRemoved;
+    char data[MAXCHAR];
+    bool isRemoved;
 
-	Dictionary() {
-		word.clear();
-		isRemoved = false;
-	}
+    void init() {
+        isRemoved = false;
+    }
 };
 
-int dictIdx[MAXCHAR];
+HeapNode charArr[MAXWORD];
+int cIdx;
+
+class Heap {
+public:
+    int arr[MAXWORD];
+    int length;
+
+    void init() {
+        length = 0;
+    }
+
+    bool compare(int parent, int child) {
+        if (strcmp(charArr[arr[parent]].data, charArr[arr[child]].data) > 0) { return true; }
+        return false;
+    }
+
+    void push(int a) {
+        int idx = length;
+        arr[length++] = a;
+
+        while ((idx - 1) / 2 >= 0 && compare((idx - 1) / 2, idx)) {
+            int temp = arr[idx];
+            arr[idx] = arr[(idx - 1) / 2];
+            arr[(idx - 1) / 2] = temp;
+            idx = (idx - 1) / 2;
+        }
+    }
+
+    int pop() {
+        int ans = arr[0];
+        arr[0] = arr[--length];
+
+        int idx = 0;
+        int left, right, child;
+
+        while (idx * 2 + 1 < length) {
+            left = idx * 2 + 1;
+            right = idx * 2 + 2;
+
+            if (right < length) {
+                if (compare(left, right)) { child = right; }
+                else { child = left; }
+            }
+            else { child = left; }
+
+            if (compare(idx, child)) {
+                int temp = arr[idx];
+                arr[idx] = arr[child];
+                arr[child] = temp;
+                idx = child;
+            }
+            else { break; }
+        }
+
+        return ans;
+    }
+};
+
+class Word {
+public:
+    char data[MAXCHAR];
+};
+
+Heap dictionary[ALPHABET][ALPHABET];
+Word words[20000];
 int wIdx;
-Dictionary dict[MAXCHAR][MAXDICT];
-Dictionary words[MAXWORD];
-unordered_map<string, int> hashDict;
-unordered_map<int, int> sIdx;
-unordered_map<int, int> eIdx;
 
-void init(int N, char mString[]) {
-	for (int i = 0; i < MAXCHAR; i++) {
-		dictIdx[i] = 0;
-	}
-	wIdx = 0;
-	hashDict.clear();
-	sIdx.clear();
-	eIdx.clear();
+char str1[MAXCHAR];
+char str2[MAXCHAR];
 
-	string s = "";
-	sIdx[0] = 0;
-	for (int i = 0; i < N; i++) {
-		if (mString[i] == '_') {
-			words[wIdx].word = s;
-			sIdx[i + 1] = wIdx + 1;
-			eIdx[i - 1] = wIdx;
-			wIdx++;
-			s = "";
-		}
-		else if (i == N - 1) {
-			s += mString[i];
-			words[wIdx].word = s;
-			eIdx[i] = wIdx;
-			wIdx++;
-		}
-		else {
-			s += mString[i];
-		}
-	}
+std::unordered_map<std::string, int> hashDict;
+std::unordered_map<int, int> hashStart;
+std::unordered_map<int, int> hashEnd;
 
+void init(int N, char mString[])
+{
+    hashDict.clear();
+    hashStart.clear();
+    hashEnd.clear();
+
+    cIdx = 0;
+    for (int i = 0; i < ALPHABET; i++) {
+        for (int j = 0; j < ALPHABET; j++) {
+            dictionary[i][j].init();
+        }
+    }
+
+    wIdx = 0;
+    char temp[MAXCHAR];
+    int k = 0;
+    hashStart[0] = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (mString[i] == '_') {
+            temp[k] = '\0';
+            hashEnd[i - 1] = wIdx;
+            strcpy(words[wIdx++].data, temp);
+
+            hashStart[i + 1] = wIdx;
+            k = 0;
+            continue;
+        }
+        temp[k] = mString[i];
+        k++;
+
+        if (i == N - 1) {
+            temp[k] = '\0';
+            hashEnd[i] = wIdx;
+            strcpy(words[wIdx++].data, temp);
+        }
+    }
 }
 
 void addWord(char mWord[])
 {
-	unordered_map<string, int>::iterator it = hashDict.find(mWord);
-	if (it == hashDict.end()) {
-		int key = strlen(mWord);
-		dict[key][dictIdx[key]].word = mWord;
-		dict[key][dictIdx[key]].isRemoved = false;
-		hashDict[mWord] = dictIdx[key];
-		dictIdx[key]++;
-	}
-	else {
-		int idx = hashDict[mWord];
-		int key = strlen(mWord);
-		dict[key][idx].isRemoved = false;
-	}
+    int key1 = (int) mWord[0] - 'a';
+    int key2 = (int) mWord[1] - 'a';
+
+    charArr[cIdx].init();
+    strcpy(charArr[cIdx].data, mWord);
+    dictionary[key1][key2].push(cIdx);
+    hashDict[mWord] = cIdx;
+
+    cIdx++;
 }
 
 void removeWord(char mWord[])
 {
-	int idx = hashDict[mWord];
-	int key = strlen(mWord);
-	dict[key][idx].isRemoved = true;
+    std::unordered_map<std::string, int>::iterator itr = hashDict.find(mWord);
+    charArr[itr->second].isRemoved = true;
 }
 
-int checkDiff(string word1, string word2) {
-	if (word1.size() != word2.size()) {
-		return 12;
-	}
-	
-	int ans = 0;
+bool search(int key, int loc, int idx) {
 
-	for (int i = 0; i < word1.size(); i++) {
-		if (word1[i] != word2[i]) {
-			ans++;
-		}
-	}
+    bool isExist = false;
 
-	return ans;
-}
+    for (int i = 0; i < ALPHABET; i++) {
+        int temp[MAXWORD];
+        int tIdx = 0;
 
-string modifying(int i) {
-	string s = "";
+        if (loc == 0) {
+            while (dictionary[key][i].length > 0 && !isExist) {
+                int curr = dictionary[key][i].pop();
+                if (charArr[curr].isRemoved) { continue; }
 
-	int key = words[i].word.size();
+                temp[tIdx++] = curr;
+                if (strlen(words[idx].data) != strlen(charArr[curr].data)) { continue; }
 
-	for (int j = 0; j < dictIdx[key]; j++) {
-		if (dict[key][j].isRemoved) { continue; }
-		int diff = checkDiff(words[i].word, dict[key][j].word);
-		if (diff == 12) { continue; }
+                int k = 0;
+                for (int i = 0; i < strlen(words[idx].data); i++) {
+                    if (words[idx].data[i] != charArr[curr].data[i]) { k++; }
+                }
+
+                if (k == 1) {
+                    strcpy(str1, charArr[curr].data);
+                    isExist = true;
+                }
+            }
+
+            for (int t = 0; t < tIdx; t++) {
+                dictionary[key][i].push(temp[t]);
+            }
+        }
 		
-		if (s.size() > 0) {
-			if (diff == 1 && s > dict[key][j].word) {
-				s = dict[key][j].word;
-			}
-		}
-		else {
-			if (diff == 1) {
-				s = dict[key][j].word;
-			}
-		}
-	}
-	return s;
+        else {
+            while (dictionary[i][key].length > 0 && !isExist) {
+                int curr = dictionary[i][key].pop();
+                if (charArr[curr].isRemoved) { continue; }
+
+                temp[tIdx++] = curr;
+                if (strlen(words[idx].data) != strlen(charArr[curr].data)) { continue; }
+
+                int k = 0;
+                for (int i = 0; i < strlen(words[idx].data); i++) {
+                    if (words[idx].data[i] != charArr[curr].data[i]) { k++; }
+                }
+
+                if (k == 1) {
+                    strcpy(str2, charArr[curr].data);
+                    isExist = true;
+                }
+            }
+
+            for (int t = 0; t < tIdx; t++) {
+                dictionary[i][key].push(temp[t]);
+            }
+        }
+    }
+
+    return isExist;
 }
 
+bool isModified(int idx) {
+    int temp[MAXWORD];
+    int tIdx = 0;
+
+    int key1 = (int) words[idx].data[0] - 'a';
+    int key2 = (int) words[idx].data[1] - 'a';
+
+    str1[0] = '~'; str2[0] = '~';
+
+    bool isExist1 = search(key1, 0, idx);
+    bool isExist2 = search(key2, 1, idx);
+
+    if (isExist1 || isExist2) {
+        if (strcmp(str1, str2) < 0) {
+            strcpy(words[idx].data, str1);
+        }
+        else {
+            strcpy(words[idx].data, str2);
+        }
+    }
+
+    return isExist1 || isExist2;
+}
 
 int correct(int mStart, int mEnd)
 {
-	int st = sIdx[mStart];
-	int ed = eIdx[mEnd];
+    int ans = 0;
 
-	int ans = 0;
-	int diff;
+    std::unordered_map<int, int>::iterator itr_st = hashStart.find(mStart);
+    std::unordered_map<int, int>::iterator itr_ed = hashEnd.find(mEnd);
 
-	for (int i = st; i <= ed; i++) {
+    for (int i = itr_st->second; i <= itr_ed->second; i++) {
+        std::unordered_map<std::string, int>::iterator itr_str = hashDict.find(words[i].data);
+        if (itr_str == hashDict.end()) {
+            if (isModified(i)) { ans++; }
+        }
+        else if (charArr[itr_str->second].isRemoved) {
+            if (isModified(i)) { ans++; }
+        }
+    }
 
-		unordered_map<string, int>::iterator it = hashDict.find(words[i].word);
-		if (it == hashDict.end()) {
-			string s = modifying(i);
-			if (s.size() > 0) {
-				words[i].word = s;
-				ans++;
-			}
-		}
-		else {
-			int idx = hashDict[words[i].word];
-			int key = words[i].word.size();
-			if (dict[key][idx].isRemoved) {
-				string s = modifying(i);
-				if (s.size() > 0) {
-					words[i].word = s;
-					ans++;
-				}
-			}
-			else { continue; }
-		}
-	}
-	return ans;
-
+    return ans;
 }
 
 void destroy(char mResult[])
 {
-	int k = 0;
-	for (int i = 0; i < wIdx; i++) {
-		for (int j = 0; j < words[i].word.size(); j++) {
-			mResult[k] = words[i].word[j];
-			k++;
-		}
-		mResult[k] = '_';
-		k++;
-	}
-	mResult[k] = '\0';
+    int k = 0;
+    for (int i = 0; i < wIdx; i++) {
+        for (int j = 0; j < strlen(words[i].data); j++) {
+            mResult[k] = words[i].data[j];
+            k++;
+        }
+        mResult[k] = '_';
+        k++;
+    }
+    mResult[k] = '\0';
 }
