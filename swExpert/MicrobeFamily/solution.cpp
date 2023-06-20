5e0330fe7172d766df9dd1a0bbd81fb338c0fd35
jonghwan83 <jonghwan.park83@gmail.com>
2023-06-20 21:39:04 +0900

    solved

--- swExpert/MicrobeFamily/solution.cpp
+++ swExpert/MicrobeFamily/solution.cpp
@@ -1,17 +1,25 @@
 #include <cstring>
+#include <cmath>
 
 using namespace std;
 
 #define MAXMICROBE 12001
 #define SUBSET 1000
 #define TABLESIZE 4096
+#define DEPTH 12
 
 
 class Microbe {
 public:
     char name[12];
-    int parent;
+    int parent[DEPTH];
     int dist;
+
+    void init() {
+        for (int i = 0; i < DEPTH; i++) {
+            parent[i] = 0;
+        }
+    }
 };
 
 class Grid {
@@ -33,10 +41,10 @@ public:
     }
 };
 
-int char2key(const char *str){
+int char2key(const char* str) {
     unsigned long hash = 5381;
     int c;
- 
+
     while (c = *str++)
     {
         hash = ((hash << 5) + hash) + c;
@@ -92,37 +100,38 @@ Grid grid[SUBSET + 1];
 
 int commonAncestor;
 
-
-void getLowestCommon(int idx1, int idx2) {
-    if (idx1 == 0 || idx2 == 0) {
-        commonAncestor = 0;
-        return;
-    }
-    if (idx1 == idx2) {
-        commonAncestor = idx1;
-        return;
-    }
-
-    if (microbes[idx1].dist > microbes[idx2].dist) {
-        getLowestCommon(microbes[idx1].parent, idx2);
-    }
-    else {
-        getLowestCommon(idx1, microbes[idx2].parent);
-    }
-}
-
-
 void LCA(int idx1, int idx2) {
-    while (idx1 != idx2) {
-        if (microbes[idx1].dist > microbes[idx2].dist) {
-            idx1 = microbes[idx1].parent;
-        }
-        else {
-            idx2 = microbes[idx2].parent;
+
+    int target = idx1; int cmp = idx2;
+    if (microbes[idx2].dist > microbes[idx1].dist) { target = idx2; cmp = idx1; }
+
+
+    if (microbes[target].dist != microbes[cmp].dist) {
+        for (int i = DEPTH - 1; i >= 0; i--) {
+
+            if (microbes[microbes[target].parent[i]].dist >= microbes[cmp].dist) {
+                target = microbes[target].parent[i];
+            }
+
         }
     }
+    
 
-    commonAncestor = idx1;
+    commonAncestor = target;
+
+    if (target != cmp) {
+        
+        for (int i = DEPTH - 1; i >= 0; i--) {
+            
+            if (microbes[target].parent[i] != microbes[cmp].parent[i]) {
+                target = microbes[target].parent[i];
+                cmp = microbes[cmp].parent[i];
+            }
+            commonAncestor = microbes[target].parent[i];
+        }
+
+    }
+
 }
 
 void addGrid(int mFirstDay, int mLastDay) {
@@ -140,7 +149,7 @@ void addGrid(int mFirstDay, int mLastDay) {
         grid[sIdx].push(st, ed);
     }
     else {
-        grid[sIdx].push(st, SUBSET-1);
+        grid[sIdx].push(st, SUBSET - 1);
         grid[eIdx].push(0, ed);
     }
 }
@@ -152,9 +161,10 @@ void init(char mAncestor[], int mLastday)
 
     mIdx = 0;
 
+    microbes[mIdx].init();
+
     strcpy(microbes[mIdx].name, mAncestor);
     microbes[mIdx].dist = 0;
-    microbes[mIdx].parent = -1;
 
     addHash(mAncestor, 0);
 
@@ -171,16 +181,23 @@ void init(char mAncestor[], int mLastday)
 }
 
 
-
 int add(char mName[], char mParent[], int mFirstday, int mLastday)
 {
     addHash(mName, mIdx);
-    
+
+    microbes[mIdx].init();
+
     int parent = findHash(mParent);
 
     strcpy(microbes[mIdx].name, mName);
-    microbes[mIdx].parent = parent;
     microbes[mIdx].dist = microbes[parent].dist + 1;
+
+    microbes[mIdx].parent[0] = parent;
+    for (int i = 1; i < DEPTH; i++) {
+        microbes[mIdx].parent[i] = microbes[parent].parent[i - 1];
+        parent = microbes[parent].parent[i - 1];
+        if (microbes[mIdx].parent[i] == 0) { break; }
+    }
 
     mIdx++;
 
