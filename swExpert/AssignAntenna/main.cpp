b3bb7aea5048c46ca6a951fe656897e6444e43c9
jonghwan83 <jonghwan.park83@gmail.com>
2024-03-23 20:42:32 +0900

    not enough

--- swExpert/AssignAntenna/main.cpp
+++ swExpert/AssignAntenna/main.cpp
@@ -10,6 +10,7 @@
 
 #include <string.h>
 
+#include <time.h>
 
 
 #define ABS(x) ((x<0)?(-(x)):(x))
@@ -23,11 +24,11 @@ static unsigned long long seed = 5;
 static int psuedo_rand(void)
 
 {
-
-	seed = seed * 25214903917ULL + 11ULL;
-
-	return (seed >> 16) & 0x3fffffff;
-
+    
+    seed = seed * 25214903917ULL + 11ULL;
+    
+    return (seed >> 16) & 0x3fffffff;
+    
 }
 
 
@@ -59,39 +60,39 @@ static const int dx[DIR_NUM] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
 struct Coordinates
 
 {
-
-	int y, x;
-
-
-
-	Coordinates()
-
-	{
-
-		y = x = 0;
-
-	}
-
-	Coordinates(int _y, int _x)
-
-	{
-
-		y = _y;
-
-		x = _x;
-
-	}
-
-
-
-	int operator-(Coordinates& param)
-
-	{
-
-		return ABS(y - param.y) + ABS(x - param.x);
-
-	}
-
+    
+    int y, x;
+    
+    
+    
+    Coordinates()
+    
+    {
+        
+        y = x = 0;
+        
+    }
+    
+    Coordinates(int _y, int _x)
+    
+    {
+        
+        y = _y;
+        
+        x = _x;
+        
+    }
+    
+    
+    
+    int operator-(Coordinates& param)
+    
+    {
+        
+        return ABS(y - param.y) + ABS(x - param.x);
+        
+    }
+    
 };
 
 
@@ -129,157 +130,157 @@ static int antenna_UE_connection[UE_NUM];
 static bool run()
 
 {
-
-	memset(antenna_ref_count, 0, sizeof(antenna_ref_count));
-
-
-
-	for (int i = 0; i < ANTENNA_NUM; i++)
-
-		antenna_list_bak[i] = antenna_list[i] = Coordinates(psuedo_rand() % MAP_SIZE, psuedo_rand() % MAP_SIZE);
-
-
-
-	init(antenna_list_bak);
-
-
-
-	for (int k = 0; k < UE_NUM; k++)
-
-	{
-
-		UE_list[k] = Coordinates(psuedo_rand() % MAP_SIZE, psuedo_rand() % MAP_SIZE);
-
-		UE_move_direction[k] = psuedo_rand() % DIR_NUM;
-
-	}
-
-
-
-	for (int t = 0; t < SUBTASK_COUNT; t++)
-
-	{
-
-		for (int k = 0; k < UE_NUM; k++)
-
-			UE_list_bak[k] = UE_list[k];
-
-
-
-		scanUE(UE_list_bak, antenna_range, antenna_UE_connection);
-
-
-
-		for (int i = 0; i < ANTENNA_NUM; ++i)
-
-		{
-
-			if (antenna_range[i] > 200)
-
-				return false;
-
-
-
-			antenna_ref_count[i] = 0;
-
-		}
-
-
-
-		for (int k = 0; k < UE_NUM; k++)
-
-		{
-
-			int i = antenna_UE_connection[k];
-
-			if (i < 0 || i >= ANTENNA_NUM)
-
-				return false;
-
-
-
-			antenna_ref_count[i]++;
-
-		}
-
-
-
-		for (int i = 0; i < ANTENNA_NUM; i++)
-
-		{
-
-			if (antenna_ref_count[i] > ANTENNA_CAPA)
-
-				return false;
-
-		}
-
-
-
-		for (int minutes = 0; minutes < 5; minutes++)
-
-		{
-
-			for (int k = 0; k < UE_NUM; k++)
-
-			{
-
-				int i = antenna_UE_connection[k];
-
-				Coordinates& antenna = antenna_list[i];
-
-				Coordinates& ue = UE_list[k];
-
-
-
-				if (antenna_range[i] < antenna - ue)
-
-					return false;
-
-			}
-
-
-
-			for (int k = 0; k < UE_NUM; k++)
-
-			{
-
-				int& dir = UE_move_direction[k];
-
-
-
-				if (dy[dir] == dx[dir])
-
-					continue;
-
-
-
-				if (UE_list[k].y + dy[dir] < 0 || UE_list[k].x + dx[dir] < 0 || UE_list[k].y + dy[dir] >= MAP_SIZE || UE_list[k].x + dx[dir] >= MAP_SIZE)
-
-				{
-
-					dir = (dir + 10) % DIR_NUM;
-
-				}
-
-
-
-				UE_list[k].y += dy[dir];
-
-				UE_list[k].x += dx[dir];
-
-			}
-
-		}
-
-		for (int i = 0; i < ANTENNA_NUM; i++)
-
-			SCORE += (antenna_range[i] * antenna_range[i]);
-
-	}
-
-	return true;
-
+    
+    memset(antenna_ref_count, 0, sizeof(antenna_ref_count));
+    
+    
+    
+    for (int i = 0; i < ANTENNA_NUM; i++)
+        
+        antenna_list_bak[i] = antenna_list[i] = Coordinates(psuedo_rand() % MAP_SIZE, psuedo_rand() % MAP_SIZE);
+    
+    
+    
+    init(antenna_list_bak);
+    
+    
+    
+    for (int k = 0; k < UE_NUM; k++)
+        
+    {
+        
+        UE_list[k] = Coordinates(psuedo_rand() % MAP_SIZE, psuedo_rand() % MAP_SIZE);
+        
+        UE_move_direction[k] = psuedo_rand() % DIR_NUM;
+        
+    }
+    
+    
+    
+    for (int t = 0; t < SUBTASK_COUNT; t++)
+        
+    {
+        
+        for (int k = 0; k < UE_NUM; k++)
+            
+            UE_list_bak[k] = UE_list[k];
+        
+        
+        
+        scanUE(UE_list_bak, antenna_range, antenna_UE_connection);
+        
+        
+        
+        for (int i = 0; i < ANTENNA_NUM; ++i)
+            
+        {
+            
+            if (antenna_range[i] > 200)
+                
+                return false;
+            
+            
+            
+            antenna_ref_count[i] = 0;
+            
+        }
+        
+        
+        
+        for (int k = 0; k < UE_NUM; k++)
+            
+        {
+            
+            int i = antenna_UE_connection[k];
+            
+            if (i < 0 || i >= ANTENNA_NUM)
+                
+                return false;
+            
+            
+            
+            antenna_ref_count[i]++;
+            
+        }
+        
+        
+        
+        for (int i = 0; i < ANTENNA_NUM; i++)
+            
+        {
+            
+            if (antenna_ref_count[i] > ANTENNA_CAPA)
+                
+                return false;
+            
+        }
+        
+        
+        
+        for (int minutes = 0; minutes < 5; minutes++)
+            
+        {
+            
+            for (int k = 0; k < UE_NUM; k++)
+                
+            {
+                
+                int i = antenna_UE_connection[k];
+                
+                Coordinates& antenna = antenna_list[i];
+                
+                Coordinates& ue = UE_list[k];
+                
+                
+                
+                if (antenna_range[i] < antenna - ue)
+                    
+                    return false;
+                
+            }
+            
+            
+            
+            for (int k = 0; k < UE_NUM; k++)
+                
+            {
+                
+                int& dir = UE_move_direction[k];
+                
+                
+                
+                if (dy[dir] == dx[dir])
+                    
+                    continue;
+                
+                
+                
+                if (UE_list[k].y + dy[dir] < 0 || UE_list[k].x + dx[dir] < 0 || UE_list[k].y + dy[dir] >= MAP_SIZE || UE_list[k].x + dx[dir] >= MAP_SIZE)
+                    
+                {
+                    
+                    dir = (dir + 10) % DIR_NUM;
+                    
+                }
+                
+                
+                
+                UE_list[k].y += dy[dir];
+                
+                UE_list[k].x += dx[dir];
+                
+            }
+            
+        }
+        
+        for (int i = 0; i < ANTENNA_NUM; i++)
+            
+            SCORE += (antenna_range[i] * antenna_range[i]);
+        
+    }
+    
+    return true;
+    
 }
 
 
@@ -287,32 +288,37 @@ static bool run()
 int main()
 
 {
-
-	setbuf(stdout, NULL);
-
-
-
-	for (int tc = 1; tc <= TC_COUNT; ++tc)
-
-	{
-
-		if (run() == false)
-
-		{
-
-			printf("SCORE: %lld\n", PENALTY);
-
-			return 0;
-
-		}
-
-	}
-
-
-
-	printf("SCORE: %lld\n", SCORE);
-
-	return 0;
-
+    clock_t start, end;
+    
+    start = clock();
+    
+    setbuf(stdout, NULL);
+    
+    
+    
+    for (int tc = 1; tc <= TC_COUNT; ++tc)
+        
+    {
+        
+        if (run() == false)
+            
+        {
+            
+            printf("SCORE: %lld\n", PENALTY);
+            
+            return 0;
+            
+        }
+        
+    }
+    
+    printf("SCORE: %lld\n", SCORE);
+    
+    end = clock() - start;
+    
+    printf("elapsed: %f\n", (float) end / CLOCKS_PER_SEC);
+    
+    return 0;
+    
 }
 
--- swExpert/AssignAntenna/solution.cpp
+++ swExpert/AssignAntenna/solution.cpp
@@ -8,184 +8,108 @@ static const int ANTENNA_CAPA = 100;
 
 static const int UE_NUM = 10'000;
 
-int max(int a, int b) {
-    if (a > b) { return a; }
-    return b;
-}
-
-int min(int a, int b) {
-    if (a > b) { return b; }
-    return a;
-}
+int max(int a, int b) { return a > b? a : b; }
 
 struct Coordinates
 
 {
-
+    
     int y, x;
-
-
-
+    
     Coordinates()
-
+    
     {
-
+        
         y = x = 0;
-
+        
     }
-
+    
     Coordinates(int _y, int _x)
-
+    
     {
-
+        
         y = _y;
-
+        
         x = _x;
-
+        
     }
-
-
-
+    
     int operator-(Coordinates& param)
-
+    
     {
-
+        
         return ABS(y - param.y) + ABS(x - param.x);
-
+        
     }
-
-};
-
-struct HeapNode {
-    int row;
-    int col;
-    int id;
-};
-
-class Heap {
-public:
-    int length;
-    HeapNode arr[ANTENNA_NUM];
-    Coordinates loc;
-
-    void init() { length = 0; }
-
-    bool compare(int parent, int child) {
-        Coordinates p = Coordinates(arr[parent].row, arr[parent].col);
-        Coordinates c = Coordinates(arr[child].row, arr[child].col);
-        if (loc - p > loc - c) { return true; }
-        return false;
-    }
-
-    void push(int r, int c, int i) {
-        HeapNode node = { r, c, i };
-
-        int idx = length;
-        arr[length++] = node;
-
-        while ((idx - 1) / 2 >= 0 && compare((idx - 1) / 2, idx)) {
-            HeapNode temp = arr[idx];
-            arr[idx] = arr[(idx - 1) / 2];
-            arr[(idx - 1) / 2] = temp;
-            idx = (idx - 1) / 2;
-        }
-    }
-
-    HeapNode pop() {
-        HeapNode ans = arr[0];
-
-        arr[0] = arr[--length];
-
-        int idx = 0;
-        int left, right, child;
-
-        while (idx * 2 + 1 < length) {
-            left = idx * 2 + 1;
-            right = idx * 2 + 2;
-
-            if (right < length) {
-                if (compare(left, right)) { child = right; }
-                else { child = left; }
-            }
-            else { child = left; }
-
-            if (compare(idx, child)) {
-                HeapNode temp = arr[idx];
-                arr[idx] = arr[child];
-                arr[child] = temp;
-                idx = child;
-            }
-            else { break; }
-        }
-
-        return ans;
-    }
+    
 };
 
 
-Heap pQueue[MAP_SIZE][MAP_SIZE];
+Coordinates antenna_list[ANTENNA_NUM];
 
-int cnt[ANTENNA_NUM];
+int capa[ANTENNA_NUM];
+
+int subtask;
 
 
-void init(Coordinates antenna_list[])
+
+void init(Coordinates _antenna_list[])
 {
-    for (int r = 0; r < MAP_SIZE; r++) {
-        for (int c = 0; c < MAP_SIZE; c++) {
-
-            pQueue[r][c].init();
-            pQueue[r][c].loc = Coordinates(r, c);
-
-            for (int i = 0; i < ANTENNA_NUM; i++) {
-                pQueue[r][c].push(antenna_list[i].y, antenna_list[i].x, i);
-            }
-
-        }
+    subtask = 0;
+    
+    for (int i = 0; i < ANTENNA_NUM; i++)
+    {
+        antenna_list[i] = _antenna_list[i];
     }
-
+    
 }
 
 
 
 void scanUE(Coordinates UE_list[], int antenna_range[], int assign_antenna[])
 {
-
-    HeapNode temp[ANTENNA_NUM];
-    int tIdx;
-
-    for (int i = 0; i < ANTENNA_NUM; i++) {
-        cnt[i] = 0;
+    for (int i = 0; i < ANTENNA_NUM; i++)
+    {
+        capa[i] = 0;
+     
+        antenna_range[i] = 0;
     }
-
-    for (int i = 0; i < UE_NUM; i++) {
-
-        tIdx = 0;
-
-        int row = UE_list[i].y; int col = UE_list[i].x;
-
-        while (pQueue[row][col].length > 0) {
-            HeapNode curr = pQueue[row][col].pop();
-
-            temp[tIdx++] = curr;
-
-            if (cnt[curr.id] >= ANTENNA_CAPA) { continue; }
-
-            cnt[curr.id]++;
-
-            assign_antenna[i] = curr.id;
-
-            Coordinates ant = Coordinates(curr.row, curr.col);
-
-            antenna_range[curr.id] = max(antenna_range[curr.id], pQueue[row][col].loc - ant + 4);
-            antenna_range[curr.id] = min(antenna_range[curr.id], 199);
-
-            break;
+    
+    
+    for (int ue = 0; ue < UE_NUM; ue++)
+    {
+        int antenna_id = -1;
+     
+        int min_dist = MAP_SIZE * MAP_SIZE * ANTENNA_NUM;
+        
+        for (int antenna = 0; antenna < ANTENNA_NUM; antenna++)
+        {
+            if (capa[antenna] >= ANTENNA_CAPA) { continue; }
+            
+            int dist = antenna_list[antenna] - UE_list[ue];
+            
+            if (antenna_id > -1 && dist + 4 < antenna_range[antenna] && capa[antenna] < capa[antenna_id])
+            {
+                min_dist = dist;
+                
+                antenna_id = antenna;
+            }
+            
+            else if (dist < min_dist)
+            {
+                min_dist = dist;
+                
+                antenna_id = antenna;
+            }
+            
         }
-
-        for (int t = 0; t < tIdx; t++) {
-            pQueue[row][col].push(temp[t].row, temp[t].col, temp[t].id);
-        }
-
+        
+        assign_antenna[ue] = antenna_id;
+        
+        antenna_range[antenna_id] = max(antenna_range[antenna_id], min_dist + 4);
+        
+        capa[antenna_id]++;
     }
-
+    
+    subtask++;
 }
