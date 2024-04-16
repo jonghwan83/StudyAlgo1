e3878d0b9c11b788737cdb0434e48ddade8a69bd
jonghwan83 <jonghwan.park83@gmail.com>
2024-04-16 21:22:46 +0900

    SCORE: 436067446

--- swExpert/TaxiAssignment/solution.cpp
+++ swExpert/TaxiAssignment/solution.cpp
@@ -36,6 +36,82 @@ int getDistance(Coordinate a, Coordinate b)
 extern bool assign_driver(int driverID, int passengerSize, int passengerIDs[]);
 
 
+struct Node
+{
+    int distance;
+    int id;
+};
+
+class Heap
+{
+public:
+    int length;
+    Node arr[MAX_PASSENGER_COUNT];
+    
+    void init() { length = 0; }
+    
+    bool compare(int parent, int child)
+    {
+        if (arr[parent].distance > arr[child].distance) { return true; }
+        return false;
+    }
+    
+    void push(int dist, int id)
+    {
+        Node node = { dist, id };
+        
+        int idx = length;
+        arr[length++] = node;
+        
+        while ((idx - 1) / 2 >= 0 && compare((idx - 1) / 2, idx))
+        {
+            Node temp = arr[idx];
+            arr[idx] = arr[(idx - 1) / 2];
+            arr[(idx - 1) / 2] = temp;
+            idx = (idx - 1) / 2;
+        }
+    }
+    
+    Node pop()
+    {
+        Node ans = arr[0];
+        
+        arr[0] = arr[--length];
+        
+        int idx = 0;
+        int left, right, child;
+        
+        while (2 * idx + 1 < length)
+        {
+            left = 2 * idx + 1;
+            right = 2 * idx + 2;
+            
+            if (right < length)
+                if (compare(left, right))
+                    child = right;
+                else
+                    child = left;
+            else
+                child = left;
+            
+            if (compare(idx, child))
+            {
+                Node temp = arr[idx];
+                arr[idx] = arr[child];
+                arr[child] = temp;
+                idx = child;
+            }
+            else { break; }
+        }
+        
+        return ans;
+    }
+};
+
+
+Heap pQueue[MAX_DRIVER_COUNT];
+
+bool finished[MAX_PASSENGER_COUNT];
 
 
 int passengerSize[MAX_DRIVER_COUNT];
@@ -49,50 +125,138 @@ int score[MAX_DRIVER_COUNT];
 void run(int N, int M, Coordinate mDriver[], int K, Passenger mPassenger[])
 {
     
-    for (int driver = 0; driver < MAX_DRIVER_COUNT; driver++)
+    
+//    for (int driver = 0; driver < M; driver++)
+//    {
+//        passengerSize[driver] = 0;
+//        
+//        driverLocation[driver] = mDriver[driver];
+//        
+//        score[driver] = 0;
+//        
+//        pQueue[driver].init();
+//    }
+    
+    
+    
+    for (int driver = 0; driver < M; driver++)
     {
         passengerSize[driver] = 0;
         
         driverLocation[driver] = mDriver[driver];
         
         score[driver] = 0;
+        
+        pQueue[driver].init();
+        
+        for (int passenger = 0; passenger < K; passenger++)
+        {
+            int dist = getDistance(mDriver[driver], mPassenger[passenger].departure);
+            
+            pQueue[driver].push(dist, passenger);
+            
+            if (driver == 0)
+            {
+                finished[passenger] = 0;
+            }
+        }
     }
     
     
-    for (int passenger = 0; passenger < K; passenger++)
+    int cnt = 0;
+    
+    while (cnt < K)
     {
-        int min_dist = 4 * N;
         
-        int close_driver = -1;
+        int curr_driver = -1;
         
-        int min_score = 4 * N * 100;
+        int min_dist = 4 * N * 100;
         
         for (int driver = 0; driver < M; driver++)
         {
-            int dist = getDistance(mPassenger[passenger].departure, driverLocation[driver]);
+            int wait_passenger = pQueue[driver].arr[0].id;
             
-            if (dist < min_dist && score[driver] == 0)
-            {
-                min_dist = dist;
-                close_driver = driver;
-            }
-            else if (dist < min_dist && score[driver] < min_score && score[driver] > 0)
-            {
-                min_dist = dist;
-                close_driver = driver;
-                min_score = score[driver];
-            }
+            int travel = getDistance(mPassenger[wait_passenger].departure, mPassenger[wait_passenger].arrival);
             
+            if (pQueue[driver].arr[0].distance + score[driver] + travel < min_dist)
+            {
+                min_dist = pQueue[driver].arr[0].distance + score[driver] + travel;
+                curr_driver = driver;
+            }
         }
         
-        passengerIDs[close_driver][passengerSize[close_driver]++] = passenger;
         
-        score[close_driver] += (getDistance(mPassenger[passenger].departure, mPassenger[passenger].arrival)
-                                + getDistance(driverLocation[close_driver], mPassenger[passenger].departure));
+        Node curr = pQueue[curr_driver].pop();
         
-        driverLocation[close_driver] = mPassenger[passenger].arrival;
+        int passenger = curr.id;
+        
+        while (finished[passenger])
+        {
+            curr = pQueue[curr_driver].pop();
+            
+            passenger = curr.id;
+        }
+        
+        passengerIDs[curr_driver][passengerSize[curr_driver]++] = passenger;
+        
+        score[curr_driver] += (getDistance(mPassenger[passenger].departure, mPassenger[passenger].arrival)
+                                + getDistance(driverLocation[curr_driver], mPassenger[passenger].departure));
+        
+        driverLocation[curr_driver] = mPassenger[passenger].arrival;
+        
+        finished[passenger] = 1;
+        
+        cnt++;
+        
+        pQueue[curr_driver].init();
+        
+        for (int p = 0; p < K; p++)
+        {
+            if (!finished[p])
+            {
+                int dist = getDistance(driverLocation[curr_driver], mPassenger[p].departure);
+                
+                pQueue[curr_driver].push(dist, p);
+            }
+        }
         
     }
+    
+    
+//    for (int passenger = 0; passenger < K; passenger++)
+//    {
+//        int min_dist = 4 * N;
+//        
+//        int close_driver = -1;
+//        
+//        int min_score = 4 * N * 100;
+//        
+//        for (int driver = 0; driver < M; driver++)
+//        {
+//            int dist = getDistance(mPassenger[passenger].departure, driverLocation[driver]);
+//            
+//            if (dist < min_dist && score[driver] == 0)
+//            {
+//                min_dist = dist;
+//                close_driver = driver;
+//            }
+//            else if (dist < min_dist && score[driver] < min_score && score[driver] > 0)
+//            {
+//                min_dist = dist;
+//                close_driver = driver;
+//                min_score = score[driver];
+//            }
+//            
+//        }
+//        
+//        passengerIDs[close_driver][passengerSize[close_driver]++] = passenger;
+//        
+//        score[close_driver] += (getDistance(mPassenger[passenger].departure, mPassenger[passenger].arrival)
+//                                + getDistance(driverLocation[close_driver], mPassenger[passenger].departure));
+//        
+//        driverLocation[close_driver] = mPassenger[passenger].arrival;
+//        
+//    }
     
     
     int tc_score = 0;
